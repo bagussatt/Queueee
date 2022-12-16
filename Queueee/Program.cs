ead0f9a6edc00ef85fdd106a231dd0eca054ad39
Bagus Satrio <bagus.satrio.ft21@mail.umy.ac.id>
2022-12-16 16:59:28 +0700

    membuat class queus

--- Queueee/Program.cs
+++ Queueee/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Queueee
+{
+    class Queues
+    {
+        int FRONT, REAR, max = 5;
+        int[] queue_array = new int[5];
+        public Queues()
+        {
+            FRONT = -1;
+            REAR = -1;
+        }
+    }
+}
